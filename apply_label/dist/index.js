55ad7ae2b4b3abbf544adb07f9abc65aec2cd2bd
Adam J. Richardson <fatman.uk@gmail.com>
2021-11-12 12:31:43 +0000

    Try octokit API in apply_label

--- apply_label/dist/index.js
+++ apply_label/dist/index.js
@@ -8448,9 +8448,11 @@ const github = __nccwpck_require__(4176);
 try {
   const label = core.getInput('LABEL');
   const token = core.getInput('GITHUB_PERSONAL_ACCESS_TOKEN');
-  const owner = github.context.repo.owner;
-  const repo = github.context.repo.repo;
-  const issue = github.context.issue.number;
+  const octokit = new github.GitHub(token);
+  const context = github.context;
+  const owner = context.repo.owner;
+  const repo = context.repo.repo;
+  const issue = context.issue.number;
 
   console.log(`Label: [${label}]`);
   console.log(`Token: [${token}]`);
@@ -8462,10 +8464,17 @@ try {
 //  const issId = JSON.stringify(github.context.payload.issue.id)
 //  console.log(`The event payload.issue.id: ${issId}`)
 
-  github.rest.issues.addLabels({
-    'issue_number': issue,
+//  github.rest.issues.addLabels({
+//    'issue_number': issue,
+//    'owner': owner,
+//    'repo': repo,
+//    'labels': [ label ]
+//  });
+
+  octokit.issues.update({
     'owner': owner,
     'repo': repo,
+    'issue_number': issue,
     'labels': [ label ]
   });
 } catch (error) {
--- apply_label/index.js
+++ apply_label/index.js
@@ -4,9 +4,11 @@ const github = require('@actions/github');
 try {
   const label = core.getInput('LABEL');
   const token = core.getInput('GITHUB_PERSONAL_ACCESS_TOKEN');
-  const owner = github.context.repo.owner;
-  const repo = github.context.repo.repo;
-  const issue = github.context.issue.number;
+  const octokit = new github.GitHub(token);
+  const context = github.context;
+  const owner = context.repo.owner;
+  const repo = context.repo.repo;
+  const issue = context.issue.number;
 
   console.log(`Label: [${label}]`);
   console.log(`Token: [${token}]`);
@@ -18,10 +20,17 @@ try {
 //  const issId = JSON.stringify(github.context.payload.issue.id)
 //  console.log(`The event payload.issue.id: ${issId}`)
 
-  github.rest.issues.addLabels({
-    'issue_number': issue,
+//  github.rest.issues.addLabels({
+//    'issue_number': issue,
+//    'owner': owner,
+//    'repo': repo,
+//    'labels': [ label ]
+//  });
+
+  octokit.issues.update({
     'owner': owner,
     'repo': repo,
+    'issue_number': issue,
     'labels': [ label ]
   });
 } catch (error) {
