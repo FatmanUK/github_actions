4a57611f92565f59f1f9127ad8a33eb1c1ba60c2
Adam J. Richardson <fatman.uk@gmail.com>
2021-12-07 18:49:44 +0000

    Append label instead of replacing

--- apply_label/index.js
+++ apply_label/index.js
@@ -9,18 +9,26 @@ try {
   const repo = github.context.repo.repo;
   const issue = github.context.issue.number;
 
-  console.log(`Label: [${label}]`);
+  // get current labels and set labels array
+  labels = octokit.rest.issues.listLabelsOnIssue({
+    'owner': owner,
+    'repo': repo,
+    'issue_number': issue,
+  });
+
   console.log(`Token: [${token}]`);
   console.log(`Owner: [${owner}]`);
   console.log(`Repo:  [${repo}]`);
   console.log(`Issue: [${issue}]`);
- 
+  labels.push(label);
+
+  console.log(`Labels: [${labels}]`);
   const octokit = new Octokit({ 'auth': token });
   octokit.rest.issues.update({
     'owner': owner,
     'repo': repo,
     'issue_number': issue,
-    'labels': [ label ]
+    'labels': labels
   });
 } catch (error) {
   core.setFailed(error.message);
