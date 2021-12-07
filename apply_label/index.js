8d22d9ed43169a0d64b815a25b9adf4ae8800102
Adam J. Richardson <fatman.uk@gmail.com>
2021-12-07 19:41:46 +0000

    Try to deal with JS promise nonsense

--- apply_label/index.js
+++ apply_label/index.js
@@ -13,22 +13,27 @@ try {
   console.log(`Owner: [${owner}]`);
   console.log(`Repo:  [${repo}]`);
   console.log(`Issue: [${issue}]`);
+  console.log(`Label: [${label}]`);
 
   const octokit = new Octokit({ 'auth': token });
-  var labels = octokit.rest.issues.listLabelsOnIssue({
+  octokit.rest.issues.listLabelsOnIssue({
     'owner': owner,
     'repo': repo,
     'issue_number': issue,
-  });
-  console.log(`Labels: [${labels}]`);
-  labels.push(label);
-
-  console.log(`Labels: [${labels}]`);
-  octokit.rest.issues.update({
-    'owner': owner,
-    'repo': repo,
-    'issue_number': issue,
-    'labels': labels
+  })
+  .then(({ data }) => {
+    console.log(`Data: ${data}`);
+    data.push(label);
+    console.log(`Data: ${data}`);
+    octokit.rest.issues.update({
+      'owner': owner,
+      'repo': repo,
+      'issue_number': issue,
+      'labels': labels
+    });
+  })
+  .catch(error => {
+    console.log("error", error);
   });
 } catch (error) {
   core.setFailed(error.message);
